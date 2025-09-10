6a7a13e875d40f5e019a5437b7bacd10175cdaa7
Bart Reardon <bart.reardon@csiro.au>
2025-09-10 21:31:04 +1000

    Define button1 and button2 as views and replace replicated code to reference button views instead. Fixes #525

--- dialog/Views/ButtonView.swift
+++ dialog/Views/ButtonView.swift
@@ -14,15 +14,11 @@ struct ButtonView: View {
 
     var progressSteps: CGFloat = appDefaults.timerDefaultSeconds
 
-    var button1action: String = ""
-    var buttonShellAction: Bool = false
     var buttonCentreStyle: Bool = false
 
     var defaultExit: Int32 = 0
     var cancelExit: Int32 = 2
     var infoExit: Int32 = 3
-
-    let timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect() //trigger after 4 seconds
 
     init(observedDialogContent: DialogUpdatableContent) {
         self.observedData = observedDialogContent
@@ -33,14 +29,7 @@ struct ButtonView: View {
             progressSteps = observedDialogContent.args.timerBar.value.floatValue()
         }
 
-        if observedDialogContent.args.button1ShellActionOption.present {
-            writeLog("Using button 1 shell action")
-            button1action = observedDialogContent.args.button1ShellActionOption.value
-            buttonShellAction = true
-        } else if observedDialogContent.args.button1ActionOption.present {
-            writeLog("Using button 1 action \(observedDialogContent.args.button1ActionOption.value)")
-            button1action = observedDialogContent.args.button1ActionOption.value
-        }
+        
     }
 
     var body: some View {
@@ -50,27 +39,11 @@ struct ButtonView: View {
                     .frame(width: 20)
 
                 if observedData.args.button2Option.present || observedData.args.button2TextOption.present {
-                    Button(action: {
-                        quitDialog(exitCode: appDefaults.exit2.code, observedObject: observedData)
-                    }, label: {
-                        Text(observedData.args.button2TextOption.value)
-                            .frame(minWidth: 50, alignment: .center)
-                    }
-                    )
-                    .keyboardShortcut(observedData.appProperties.button2DefaultAction)
+                    Button2(observedDialogContent: observedData)
                 }
 
                 if !(observedData.args.button1TextOption.value == "none") {
-                    Button(action: {
-                        buttonAction(action: self.button1action, exitCode: 0, executeShell: self.buttonShellAction, observedObject: observedData)
-
-                    }, label: {
-                        Text(observedData.args.button1TextOption.value)
-                            .frame(minWidth: 50, alignment: .center)
-                    }
-                    )
-                    .keyboardShortcut(observedData.appProperties.button1DefaultAction)
-                    .disabled(buttonCentreStyle ? false : observedData.args.button1Disabled.present)
+                    Button1(observedDialogContent: observedData)
                 }
                 Spacer()
                     .frame(width: 20)
@@ -78,34 +51,11 @@ struct ButtonView: View {
         } else if ["stack"].contains(observedData.args.buttonStyle.value) {
             VStack {
                 if !(observedData.args.button1TextOption.value == "none") {
-                    Button(action: {
-                        buttonAction(action: self.button1action, exitCode: 0, executeShell: self.buttonShellAction, observedObject: observedData)
-
-                    }, label: {
-                        Text(observedData.args.button1TextOption.value)
-                            .frame(minWidth: 50, maxWidth: .infinity, alignment: .center)
-                            .padding(5)
-                    }
-                    )
-                    .keyboardShortcut(observedData.appProperties.button1DefaultAction)
-                    .disabled(buttonCentreStyle ? false : observedData.args.button1Disabled.present)
-                    .onReceive(timer) { _ in
-                            if observedData.args.timerBar.present && !observedData.args.hideTimerBar.present {
-                                observedData.args.button1Disabled.present = false
-                            }
-                    }
+                    Button1(observedDialogContent: observedData)
                 }
 
                 if observedData.args.button2Option.present || observedData.args.button2TextOption.present {
-                    Button(action: {
-                        quitDialog(exitCode: appDefaults.exit2.code, observedObject: observedData)
-                    }, label: {
-                        Text(observedData.args.button2TextOption.value)
-                            .frame(minWidth: 50, maxWidth: .infinity, alignment: .center)
-                            .padding(5)
-                    }
-                    )
-                    .keyboardShortcut(observedData.appProperties.button2DefaultAction)
+                    Button2(observedDialogContent: observedData)
                 }
                 if observedData.args.timerBar.present {
                     TimerView(progressSteps: progressSteps, visible: !observedData.args.hideTimerBar.present, observedDialogContent: observedData, stacked: true)
@@ -136,16 +86,7 @@ struct ButtonView: View {
                     Spacer()
                     HStack {
                         if observedData.args.button2Option.present || observedData.args.button2TextOption.present {
-                            let button2Text: String = observedData.args.button2TextOption.value
-                            Button(action: {
-                                quitDialog(exitCode: appDefaults.exit2.code, observedObject: observedData)
-                            }, label: {
-                                Text(button2Text)
-                                    .frame(minWidth: 40, alignment: .center)
-                            }
-                            )
-                            .keyboardShortcut(observedData.appProperties.button2DefaultAction)
-                            .disabled(observedData.args.button2Disabled.present)
+                            Button2(observedDialogContent: observedData)
                         }
                     }
 
@@ -153,21 +94,7 @@ struct ButtonView: View {
                     let button1Text: String = observedData.args.button1TextOption.value
 
                     if !(observedData.args.button1TextOption.value == "none") {
-                        Button(action: {
-                            buttonAction(action: self.button1action, exitCode: 0, executeShell: self.buttonShellAction, observedObject: observedData)
-
-                        }, label: {
-                            Text(button1Text)
-                                .frame(minWidth: 40, alignment: .center)
-                        }
-                        )
-                        .keyboardShortcut(observedData.appProperties.button1DefaultAction)
-                        .disabled(observedData.args.button1Disabled.present)
-                        .onReceive(timer) { _ in
-                            if observedData.args.timerBar.present && !observedData.args.hideTimerBar.present {
-                                observedData.args.button1Disabled.present = false
-                            }
-                        }
+                        Button1(observedDialogContent: observedData)
                     }
 
                     // Help Button
@@ -230,6 +157,72 @@ struct HelpButtonStyle: ButtonStyle {
     }
 }
 
+struct Button1: View {
+    @ObservedObject var observedData: DialogUpdatableContent
+    
+    var button1action: String = ""
+    var buttonShellAction: Bool = false
+    let timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect() //trigger after 4 seconds
+    
+    init(observedDialogContent: DialogUpdatableContent) {
+        self.observedData = observedDialogContent
+        if observedDialogContent.args.button1ShellActionOption.present {
+            writeLog("Using button 1 shell action")
+            button1action = observedDialogContent.args.button1ShellActionOption.value
+            buttonShellAction = true
+        } else if observedDialogContent.args.button1ActionOption.present {
+            writeLog("Using button 1 action \(observedDialogContent.args.button1ActionOption.value)")
+            button1action = observedDialogContent.args.button1ActionOption.value
+        }
+    }
+    
+    var body: some View {
+        // default button aka button 1
+        let button1Text: String = observedData.args.button1TextOption.value
+
+        if !(observedData.args.button1TextOption.value == "none") {
+            Button(action: {
+                buttonAction(action: self.button1action, exitCode: 0, executeShell: self.buttonShellAction, observedObject: observedData)
+
+            }, label: {
+                Text(button1Text)
+                    .frame(minWidth: 40, alignment: .center)
+            }
+            )
+            .keyboardShortcut(observedData.appProperties.button1DefaultAction)
+            .disabled(observedData.args.button1Disabled.present)
+            .onReceive(timer) { _ in
+                if observedData.args.timerBar.present && !observedData.args.hideTimerBar.present {
+                    observedData.args.button1Disabled.present = false
+                }
+            }
+        }
+    }
+}
+
+struct Button2: View {
+    @ObservedObject var observedData: DialogUpdatableContent
+    
+    init(observedDialogContent: DialogUpdatableContent) {
+        self.observedData = observedDialogContent
+    }
+    
+    var body: some View {
+        HStack {
+            let button2Text: String = observedData.args.button2TextOption.value
+            Button(action: {
+                quitDialog(exitCode: appDefaults.exit2.code, observedObject: observedData)
+            }, label: {
+                Text(button2Text)
+                    .frame(minWidth: 40, alignment: .center)
+            }
+            )
+            .keyboardShortcut(observedData.appProperties.button2DefaultAction)
+            .disabled(observedData.args.button2Disabled.present)
+        }
+    }
+}
+
 struct MoreInfoButton: View {
     @ObservedObject var observedData: DialogUpdatableContent
 
@@ -259,5 +252,4 @@ struct MoreInfoButton: View {
             }
         }
     }
-
 }
